435c328ebc2ea811fc59822a2d7ea64c5b66a316
sh.inv <104422865+sh-inv@users.noreply.github.com>
2022-10-17 22:30:54 +0900

    Feature/scrolltotop (#6)
    * Develop/merge (#2)
    
    * Add: 로그인 ui 및 기능 완료
    
    * Add: pr 템플릿 추가
    
    * Add: scroll to top UI 및 기능 구현

--- src/components/ScrollToTopBtn/index.jsx
+++ src/components/ScrollToTopBtn/index.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 function ScrollToTopBtn() {
-  const [showButton, setShowButton] = useState(false);
+  const location = useLocation();
 
   const scrollToTop = () => {
     window.scroll({
@@ -11,15 +11,7 @@ function ScrollToTopBtn() {
     });
   };
 
-  useEffect(() => {
-    const handleShowButton = () => (window.scrollY ? setShowButton(true) : setShowButton(false));
-    window.addEventListener('scroll', handleShowButton);
-    return () => {
-      window.removeEventListener('scroll', handleShowButton);
-    };
-  }, []);
-
-  return showButton && <Button onClick={scrollToTop} />;
+  return location.pathname !== '/' && <Button onClick={scrollToTop} />;
 }
 
 const Button = styled.button`
@@ -29,8 +21,9 @@ const Button = styled.button`
   width: 64px;
   height: 64px;
   border: none;
-  background: url('https://hyper-cloud.kr/static/images/scroll_top.png') center center no-repeat;
+  background: url('https://user-images.githubusercontent.com/104422865/196187367-b8b65a1a-4976-4cd7-825b-f1a786f88ad8.png') center center no-repeat;
   background-size: cover;
+  transform: rotate(270deg);
   color: #ffffff;
   cursor: pointer;
   z-index: 10;
